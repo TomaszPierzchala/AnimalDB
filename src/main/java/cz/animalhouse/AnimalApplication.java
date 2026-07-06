85b2df9ff8f31410fcbcd6be538908eedd591cdf
Tomasz Pierzchała <t.pierzchala@gmail.com>
2026-07-06 19:10:49 +0200

    modified:   src/main/java/cz/animalhouse/AnimalApplication.java

--- src/main/java/cz/animalhouse/AnimalApplication.java
+++ src/main/java/cz/animalhouse/AnimalApplication.java
@@ -1,13 +1,13 @@
-package cz.animalhouse.demo;
+package cz.animalhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AnimalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AnimalApplication.class, args);
 	}
 
 }
